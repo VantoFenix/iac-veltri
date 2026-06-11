c3d6396e3c71712b44ef62ac65e25e65728520d4
bruno <vantofortnite@gmail.com>
2026-06-11 17:48:36 -0500

    feat:orquestacion principal y llamado a modulos de arquitectura

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+module "network" {
+  source     = "./modules/network"
+  aws_region = var.aws_region
+  proyecto   = var.proyecto
+  ambiente   = var.ambiente
+}
+
+module "data" {
+  source     = "./modules/data"
+  proyecto   = var.proyecto
+  ambiente   = var.ambiente
+}
+
+module "compute" {
+  source     = "./modules/compute"
+  proyecto   = var.proyecto
+  ambiente   = var.ambiente
+}
+
+module "edge" {
+  source     = "./modules/edge"
+  proyecto   = var.proyecto
+  ambiente   = var.ambiente
+}
