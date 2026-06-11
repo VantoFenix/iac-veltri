d03b70ef428cdf0bec1726a76d35576f1e1ff58c
bruno <vantofortnite@gmail.com>
2026-06-11 18:40:58 -0500

    feat:implementacion de nat gateways y tablas de ruteo publicas

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -62,3 +62,38 @@ resource "aws_subnet" "private_6_data" {
   availability_zone = "${var.aws_region}b"
   tags              = { Name = "${var.proyecto}-${var.ambiente}-private-data-6" }
 }
+# 6 IPs Elasticas y NAT Gateways
+resource "aws_eip" "nat_1" { domain = "vpc" }
+resource "aws_eip" "nat_2" { domain = "vpc" }
+
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat_1.id
+  subnet_id     = aws_subnet.public_1.id
+  tags          = { Name = "${var.proyecto}-${var.ambiente}-nat-gw-1" }
+}
+
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat_2.id
+  subnet_id     = aws_subnet.public_2.id
+  tags          = { Name = "${var.proyecto}-${var.ambiente}-nat-gw-2" }
+}
+
+# 7 Tablas de Enrutamiento Publicas
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "${var.proyecto}-${var.ambiente}-public-rt" }
+}
+
+resource "aws_route_table_association" "pub_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "pub_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
