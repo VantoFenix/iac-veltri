d75cd66404dfcb6b93d3bd2ebc5bacc75168fae7
bruno <vantofortnite@gmail.com>
2026-06-11 18:39:52 -0500

    feat:segmentacion de subredes publicas y privadas en zonas a y b

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -15,3 +15,50 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "${var.proyecto}-${var.ambiente}-igw" }
 }
+
+# 3. Subredes Publicas
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.proyecto}-${var.ambiente}-public-1" }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.proyecto}-${var.ambiente}-public-2" }
+}
+
+# 4 Subredes Privadas (Capa de Computo)
+resource "aws_subnet" "private_3_compute" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags              = { Name = "${var.proyecto}-${var.ambiente}-private-compute-3" }
+}
+
+resource "aws_subnet" "private_4_compute" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags              = { Name = "${var.proyecto}-${var.ambiente}-private-compute-4" }
+}
+
+# 5 Subredes Privadas (Capa de Datos)
+resource "aws_subnet" "private_5_data" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags              = { Name = "${var.proyecto}-${var.ambiente}-private-data-5" }
+}
+
+resource "aws_subnet" "private_6_data" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags              = { Name = "${var.proyecto}-${var.ambiente}-private-data-6" }
+}
