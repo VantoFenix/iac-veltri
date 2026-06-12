aaf2ee852766e9624747ecda6dcad461e590cdc7
bruno <vantofortnite@gmail.com>
2026-06-11 19:08:22 -0500

    fix(core): actualizacion de region aws a us-east-1

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "veltri-minimarket-tfstate" 
     key     = "estado/terraform.tfstate"
-    region  = "eu-east-1"
+    region  = "us-east-1"
     encrypt = true
   }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "Region principal de despliegue en AWS"
   type        = string
-  default     = "eu-east-1"
+  default     = "us-east-1"
 }
 
 variable "proyecto" {
