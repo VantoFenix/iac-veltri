281f13a489c9bab7fdf6f88497307fcbbcfeb0f9
yoshuaalexrull-a11y <yoshua.alex.rull@gmail.com>
2026-06-17 23:07:34 -0500

    fix: resolver vulnerabilidad CKV_AWS_130 en subredes publicas

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags                    = { Name = "${var.proyecto}-${var.ambiente}-public-1" }
 }
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "${var.aws_region}b"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags                    = { Name = "${var.proyecto}-${var.ambiente}-public-2" }
 }
 
