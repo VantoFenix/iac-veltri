977c70202e6778a057c9366d2a6305610ae6657d
bruno <vantofortnite@gmail.com>
2026-06-18 22:23:37 -0500

    feat:inyeccion de dependencias de red y seguridad entre modulos

--- infra/main.tf
+++ infra/main.tf
@@ -5,20 +5,32 @@ module "network" {
   ambiente   = var.ambiente
 }
 
-module "data" {
-  source   = "./modules/data"
-  proyecto = var.proyecto
-  ambiente = var.ambiente
+module "compute" {
+  source                  = "./modules/compute"
+  proyecto                = var.proyecto
+  ambiente                = var.ambiente
+  aws_region              = var.aws_region
+  
+  vpc_id                  = module.network.vpc_id
+  public_subnets          = module.network.public_subnets
+  private_subnets_compute = module.network.private_subnets_compute
 }
 
-module "compute" {
-  source   = "./modules/compute"
-  proyecto = var.proyecto
-  ambiente = var.ambiente
+module "data" {
+  source                    = "./modules/data"
+  proyecto                  = var.proyecto
+  ambiente                  = var.ambiente
+  
+  vpc_id                    = module.network.vpc_id
+  private_subnets_data      = module.network.private_subnets_data
+  security_group_compute_id = module.compute.security_group_compute_id
 }
 
 module "edge" {
-  source   = "./modules/edge"
-  proyecto = var.proyecto
-  ambiente = var.ambiente
+  source                  = "./modules/edge"
+  proyecto                = var.proyecto
+  ambiente                = var.ambiente
+  
+  vpc_id                  = module.network.vpc_id
+  private_subnets_compute = module.network.private_subnets_compute
 }
