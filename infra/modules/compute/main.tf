dd144b8769d255c76c6e7e818a25daa109538bd6
bruno <vantofortnite@gmail.com>
2026-06-23 22:21:31 -0500

    fix: configurar encriptacion KMS en ECR

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -134,8 +134,11 @@ resource "aws_ecr_repository" "app" {
   force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
-  }
 
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
   tags = {
     Name       = "${var.proyecto}-${var.ambiente}-ecr-app"
     Modulo     = "compute"
