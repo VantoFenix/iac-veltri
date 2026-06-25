884a0649580a13809772f9efc63184e00859436f
TiagoTZ0 <tayotrigoso@gmail.com>
2026-06-24 23:54:29 -0500

    fix: CKV_AWS_313 - Asegurar que el cluster RDS copie las etiquetas a los snapshots

--- infra/modules/data/main.tf
+++ infra/modules/data/main.tf
@@ -91,6 +91,7 @@ resource "aws_rds_cluster" "aurora_cluster" {
   iam_database_authentication_enabled = true
   enabled_cloudwatch_logs_exports     = ["audit", "error", "general", "slowquery"]
   deletion_protection                 = true
+  copy_tags_to_snapshot               = true
   tags                                = { Name = "${var.proyecto}-${var.ambiente}-aurora-cluster", Modulo = "data", Ambiente = var.ambiente, Gestionado = "Terraform" }
 
   lifecycle {
